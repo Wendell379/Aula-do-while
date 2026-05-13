f01c675ab7f6c37ccf7f7db2c4ef944ab6e6abc9
WENDELL DE SOUZA <wendell.souza8@senaisp.edu.br>
2026-05-13 17:07:14 -0300

    Exercicio6

--- src/exercicio7.java
+++ src/exercicio7.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class exercicio7 {
-    void main () {
-    }
+    void main() {
+        Scanner sc = new Scanner(System.in);
+        int num2;
+        do {
+            IO.println(" 1 2 3 4 5");
+            num2 = sc.nextInt();
+            if (num2 == 1) {
+                IO.println("Somar");
+            } else if (num2 == 2) {
+                IO.println("Subtrair");
+            } else if (num2 == 3) {
+                IO.println("Multiplicar");
+            } else if (num2 == 4) {
+                IO.println("Dividir");
+            } else if (num2 == 5) {
+                IO.println("Sair");
+
+        }
+    } while (num2 != 3) ;
+}
 }
