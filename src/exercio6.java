e0a55483b0bba27c20aa9cb1cfff2b1e04318444
WENDELL DE SOUZA <wendell.souza8@senaisp.edu.br>
2026-05-13 16:55:25 -0300

    Exercicio6

--- src/exercio6.java
+++ src/exercio6.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class exercio6 {
-    void main () {
+    void main() {
         Scanner sc = new Scanner(System.in);
+        int num2;
         do {
             IO.println(" 1 2 3");
-            int num2 = sc.nextInt();
-          if (num2 == 1 ) {
-              IO.println("Olá");
-          }  else if (num2 == 2) {
-              IO.println("Maneiro");
-          }else if (num2 == 3) {
-              IO.println("nulo");
-
-          } else
-        } while (num2 != 3);
-
+            num2 = sc.nextInt();
+            if (num2 == 1) {
+                IO.println("Olá");
+            } else if (num2 == 2) {
+                IO.println("Maneiro");
+            } else if (num2 == 3) {
+                IO.println("nulo");
+            }
+        } while (num2 != 3) ;
     }
 }
